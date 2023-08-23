a049890ea814d7db241610cbf116991a69e90367
Juan diego <ranchermessi@gmail.com>
2023-08-23 01:34:42 -0500

    terminando listas

--- app/src/main/java/com/example/turistiandov2/HotelesHome.java
+++ app/src/main/java/com/example/turistiandov2/HotelesHome.java
@@ -2,13 +2,34 @@ package com.example.turistiandov2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class HotelesHome extends AppCompatActivity {
+
+    Button botonVerMas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hoteles_home);
+
+        botonVerMas= findViewById(R.id.botonvermas);
+
+        botonVerMas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentListaHotel = new Intent(HotelesHome.this, ListaHoteles.class);
+                startActivity(intentListaHotel);
+            }
+        });
+
+
+
+
     }
 }
--- app/src/main/java/com/example/turistiandov2/ListaHoteles.java
+++ app/src/main/java/com/example/turistiandov2/ListaHoteles.java
@@ -1,14 +1,43 @@
 package com.example.turistiandov2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.turistiandov2.adaptadores.AdaptadorHotel;
+import com.example.turistiandov2.adaptadores.AdaptadorRestaurante;
+import com.example.turistiandov2.moldes.MoldeHotel;
+
+import java.util.ArrayList;
+
 public class ListaHoteles extends AppCompatActivity {
+
+    ArrayList<MoldeHotel> listaHotel = new ArrayList<>();
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_hoteles);
+
+        recyclerView = findViewById((R.id.recyclerViewHoteles));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+
+        llenarListaConDatos();
+        AdaptadorHotel adaptadorHotel = new AdaptadorHotel(listaHotel);
+        recyclerView.setAdapter(adaptadorHotel);
+    }
+
+    private void llenarListaConDatos() {
+
+        listaHotel.add(new MoldeHotel("Tradiciones Café Hotel", "$80.000", "3225950033", R.drawable.h1 ));
+        listaHotel.add(new MoldeHotel("Hotel el volado", "$20.000", "3225958080", R.drawable.h2 ));
+        listaHotel.add(new MoldeHotel("Garavito Hotel", "$100.000", "3224569033", R.drawable.h3 ));
+        listaHotel.add(new MoldeHotel("Fragancia Hotel", "$120.000", "323590357", R.drawable.h4 ));
+        listaHotel.add(new MoldeHotel("Camino a santafe Hotel", "$87.000", "3225950033", R.drawable.h5 ));
+
     }
 }
